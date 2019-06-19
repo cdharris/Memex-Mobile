c93ea483fcdaebc7855de3ec4d8980deee78bbad
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-06-19 15:27:35 +0700

    Get script injection working via file load
    - very rough; more of a proof-of-concept
    - just injects a script from the FS into each page running an alert
    - needs to read the entire script into memory and pass into webview

--- app/src/features/example/ui/screens/home-screen/index.tsx
+++ app/src/features/example/ui/screens/home-screen/index.tsx
@@ -1,35 +1,73 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import RNFS from "react-native-fs";
 
-import { MemexWebView } from '../../components/web-view'
-import { AddressBar } from '../../components/address-bar'
-import { StatefulUIElement } from 'src/ui/types'
-import { injectScript } from 'src/features/content_script/script-injector'
-import Logic, { State, Event } from './logic';
-import styles from './styles'
+import { MemexWebView } from "../../components/web-view";
+import { AddressBar } from "../../components/address-bar";
+import { StatefulUIElement } from "src/ui/types";
+// import { injectScript } from "src/features/content_script/script-injector";
+import Logic, { State, Event } from "./logic";
+import styles from "./styles";
 
-interface Props {
-}
+interface Props {}
 
 export default class HomeScreen extends StatefulUIElement<Props, State, Event> {
-    constructor(props : Props) {
-        super(props, { logic: new Logic() })
+  webView = null;
+
+  constructor(props: Props) {
+    super(props, { logic: new Logic() });
+  }
+
+  async readInputFile({
+    bundleName = "ContentScript.bundle",
+    scriptName = "index.js"
+  }) {
+    const bundles = await RNFS.readDir(RNFS.MainBundlePath);
+    const contentScriptBundle = bundles.find(
+      bundle => bundle.name === bundleName
+    );
+
+    if (!contentScriptBundle) {
+      throw new Error();
     }
 
-    render() {
-        return (
-            <View style={styles.container}>
-              <AddressBar
-                onTextChange={text => this.processEvent('setAddressBarText', { text })}
-                onEnterPress={url => this.processEvent('setCurrentUrl', { url })}
-                value={this.state.addressBarText}
-              />
-              <MemexWebView
-                injectedJavaScript={injectScript({})}
-                currentUrl={this.state.currentUrl}
-                updateAddressBar={url => this.processEvent('setAddressBarText', { text: url })}
-              />
-           </View>
-        );
-      }
+    const bundleFiles = await RNFS.readDir(contentScriptBundle.path);
+    const contentScript = bundleFiles.find(
+      file => file.isFile() && file.name === scriptName
+    );
+
+    if (!contentScript) {
+      throw new Error();
+    }
+
+    return RNFS.readFile(contentScript.path);
+  }
+
+  handleLoadEnd = async e => {
+    const content = await this.readInputFile({});
+    this.webView.injectJavaScript(content);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <AddressBar
+          onTextChange={text =>
+            this.processEvent("setAddressBarText", { text })
+          }
+          onEnterPress={url => this.processEvent("setCurrentUrl", { url })}
+          value={this.state.addressBarText}
+        />
+        <MemexWebView
+          ref={el => (this.webView = el)}
+          // injectedJavaScript={injectScript({})}
+          onLoadEnd={this.handleLoadEnd}
+          currentUrl={this.state.currentUrl}
+          updateAddressBar={url =>
+            this.processEvent("setAddressBarText", { text: url })
+          }
+        />
+      </View>
+    );
+  }
 }
